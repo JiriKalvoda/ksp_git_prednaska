0c9079958cbec42e4c651a97eea2bbf473934b28
Jiri Kalvoda <jirikalvoda@kam.mff.cuni.cz>
2023-04-16 16:37:58 +0200

    Vytvářím main

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+#ifdef DEB
+#define D if(1)
+#else
+#define D if(0)
+#endif
+
+#define fo(a,b) for(int a=0;a<(b);++a)
+using ll = long long;
+
+int main(int argc, char ** argv)
+{
+	return 0;
+}
+
